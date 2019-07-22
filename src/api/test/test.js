c4d0653b2fb2ac92ebafdfc3d727bd79501241db
Nicola Cimmino <nicola.cimmino@gmail.com>
2019-07-22 13:47:42 +0100

    Add unit tests scaffolding

---
+++ src/api/test/test.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('Test', function() {
+    describe('#runs', function() {
+        it('should run', function() {
+            assert.equal(1,1);
+        });
+    });
+});
