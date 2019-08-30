3f9c3ea9889888c468c3100283c708bdfcfe7773
Nicola Cimmino <nicola.cimmino@gmail.com>
2019-08-30 15:08:42 +0100

    Refactor utils scripts

--- src/idDispenser/test/utils/decode.sh
+++ src/idDispenser/test/utils/decode.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-colorGreen="\e[32m"
-colorDefault="\e[39m"
-thisScript=`basename "$0"`
-tmpFile=`mktemp`
+. ./lib.sh
 
 if [[ "$#" -lt 2 ]]; then
     echo "Usage: ${thisScript} idtag encoded [stage=dev]"
@@ -14,13 +11,4 @@ stage=${3:-dev}
 function="awsh-iddispenser-${stage}-decode"
 payload="{ \"idtag\": \"$1\", \"encoded\": \"$2\" }"
 
-printf "${colorGreen}Invoking:${colorDefault}${function}\n"
-printf "${colorGreen}Payload:${colorDefault}${payload}\n"
-
-printf "${colorGreen}Response:${colorDefault}"
-aws lambda invoke --function-name "$function" --payload "$payload" "$tmpFile"
-
-printf "${colorGreen}Body:${colorDefault}"
-cat "$tmpFile"
-
-rm "$tmpFile"
+invokeLambda "${function}" "${payload}"
--- src/idDispenser/test/utils/dispense.sh
+++ src/idDispenser/test/utils/dispense.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-colorGreen="\e[32m"
-colorDefault="\e[39m"
-thisScript=`basename "$0"`
-tmpFile=`mktemp`
+. ./lib.sh
 
 if [[ "$#" -lt 1 ]]; then
     echo "Usage: ${thisScript} idtag [stage=dev]"
@@ -14,13 +11,4 @@ stage=${2:-dev}
 function="awsh-iddispenser-${stage}-dispense"
 payload="{ \"idtag\": \"$1\" }"
 
-printf "${colorGreen}Invoking:${colorDefault}${function}\n"
-printf "${colorGreen}Payload:${colorDefault}${payload}\n"
-
-printf "${colorGreen}Response:${colorDefault}"
-aws lambda invoke --function-name "$function" --payload "$payload" "$tmpFile"
-
-printf "${colorGreen}Body:${colorDefault}"
-cat "$tmpFile"
-
-rm "$tmpFile"
+invokeLambda "${function}" "${payload}"
